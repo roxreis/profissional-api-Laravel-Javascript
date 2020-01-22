a206deca26ee4db387355a56e50350d817af2705
roxreis <Digital@DH-A10-23.dhbrasil.digitalhouse.com>
2020-01-22 10:52:40 -0200

    criado migrations

---
+++ backend/database/migrations/2020_01_22_122515_create_profissionais.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfissionais extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profissionais', function (Blueprint $table) {
+            $table->bigIncrements('prof_id');
+            $table->string('nome');
+            $table->string('github');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profissionais');
+    }
+}
